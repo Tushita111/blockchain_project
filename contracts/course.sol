27a2a77057845cd9bfcfc17401e9c4b0b815ee8f
rutilel <119107376+rutilel@users.noreply.github.com>
2023-01-27 23:01:30 +0100

    Update course.sol
    - add global maps to store info of different courses
    
      - recordInfo: store basic information for each course
    
      - recordBid: store bid information for each course
    
    - implement a new function addCourse() to add new course to record(can be used when initializing the system or let an administrator to add new course)

--- contracts/course.sol
+++ contracts/course.sol
@@ -1,53 +1,73 @@
 // SPDX-License-Identifier: MIT
-pragma solidity^0.5.2;
+pragma solidity>=0.5.2;
 
-contract Course{
+contract CourseSelectionSystem{
     address public admin;
-    string public courseName; 
-    bool isFinshed; 
-    uint256 outTime; 
-    uint256 public Nstudent;//maximum number of student allowed in this class
+    uint public courseCount;
+    mapping(uint=>CourseInfo) public recordInfo;//store information for course
+    mapping(uint=>mapping(uint=>rankListElement)) recordBid;//store information for bid
 
-    struct rankListElement {//to store the amound of token each student bid
+    struct rankListElement {//to store the amount of token each student bid
         address addr;
         uint256 amount;
     }
 
-    rankListElement[] rankingList;//list of address->amount
-    
-    //initialization of course name, limit of duration(min), limit of #student in this course
-    constructor(string memory _name, uint256 duration, uint256 _Nstudent) public{
-        courseName=_name;
-        admin=msg.sender; 
-        outTime=block.timestamp+duration*60; //allowed duration of selection
-        isFinshed=false; 
-        Nstudent = _Nstudent;
-
-        for(uint256 i=0; i<Nstudent;i++){//initialization
-            rankingList.push(
-                rankListElement(
-                    admin,
-                    0
-                )
-            );
-        }
+    struct CourseInfo{//basic info of one course
+        uint courseId;
+        string courseName;
+        uint Nstudent;
+        uint256 startTime;
+        uint256 duration;//unit:minute
+        bool isFinshed; 
     }
 
-    function sortRankingList() public{//test done
-        for(uint256 i=0; i<Nstudent;i++){
-            for(uint256 j=0;j<Nstudent-1;j++){
-                if (rankingList[j].amount < rankingList[j+1].amount){
-                    rankListElement memory temp = rankingList[j];
-                    rankingList[j] = rankingList[j+1];
-                    rankingList[j+1] = temp;
+    //initialization of course name, limit of duration(min), limit of #student in this course
+    constructor() {
+        admin=msg.sender;
+        courseCount = 0;
+        addCourse("ALIA",5,10);
+        addCourse("DBMS",2,10);
+        // addCourse("OT4",20,10);
+    }
+
+    modifier isAdmin(){//check if the action is legal
+        if (msg.sender == admin) _;
+    }
+
+    function addCourse(string memory _name, uint _Nstudent, uint _duration) public isAdmin {
+        CourseInfo memory course = CourseInfo(
+            courseCount,
+            _name, 
+            _Nstudent, 
+            block.timestamp, 
+            _duration, 
+            false
+        );
+        recordInfo[courseCount] = course;
+
+        for(uint i=0;i<_Nstudent;i++){
+            recordBid[courseCount][i] = rankListElement(admin,0);
+        }
+
+        courseCount+=1;
+
+    }
+
+    function sortRankingList(uint _courseId) private {//used for sorting bid information
+        for(uint i=0; i<recordInfo[_courseId].Nstudent;i++){
+            for(uint j=0;j<recordInfo[_courseId].Nstudent-1;j++){
+                if (recordBid[_courseId][j].amount <recordBid[_courseId][j+1].amount){
+                    rankListElement memory temp = recordBid[_courseId][j];
+                    recordBid[_courseId][j] = recordBid[_courseId][j+1];
+                    recordBid[_courseId][j+1] = temp;
                 }
             }
         }
     }
 
-    function isInList(address studentAddr) public view returns(bool){//test done
-        for(uint256 i=0; i<Nstudent;i++){
-            if(rankingList[i].addr==studentAddr){
+    function isInList(address studentAddr, uint _courseId) private view returns(bool){//check if one student has already bid for this course
+        for(uint i=0; i<recordInfo[_courseId].Nstudent;i++){
+            if(recordBid[_courseId][i].addr==studentAddr){
                 return true;
             }
         }
@@ -55,36 +75,26 @@ contract Course{
     }
 
     // select course
-    function bid_course() public payable{
-        require(!isInList(msg.sender),"You've already bid for this course.");
-        require(msg.value>0,"You must bid at least one token.");
-        require(block.timestamp<=outTime,"Time is up.");
-        require(!isFinshed,"The course selection is finished.");
-            
-        if (msg.value > rankingList[rankingList.length-1].amount){
-            rankingList.pop();
-            rankingList.push(
-                rankListElement(
-                    msg.sender,
-                    msg.value
-                )
-            );
-            sortRankingList();
+    function bid_course(uint _courseId) public payable returns(bool){//return if the transaction succeed
+        require(!isInList(msg.sender,_courseId),"You've already bid for this course.");
+        require(msg.value>0,"You must bid a number greater than 0.");
+        require(block.timestamp<=recordInfo[_courseId].startTime+recordInfo[_courseId].duration*60,"Time is up.");
+        require(!recordInfo[_courseId].isFinshed,"The selection is finished for this course.");
+        
+        if (msg.value > recordBid[_courseId][recordInfo[_courseId].Nstudent-1].amount){
+            recordBid[_courseId][recordInfo[_courseId].Nstudent-1].addr = msg.sender;
+            recordBid[_courseId][recordInfo[_courseId].Nstudent-1].amount = msg.value;
+            sortRankingList(_courseId);
+            return true;
         }
+        return false;
     }
     
-    function endAuction() public payable{
-      require(msg.sender==admin,"only admin can do this");
-      require(block.timestamp>outTime,"time is not ok");
-      require(!isFinshed,"must not finshed");
-      isFinshed=true;
+    function endSelection(uint _courseId) public payable isAdmin {
+        CourseInfo storage courseinfo = recordInfo[_courseId];
+        require(block.timestamp>courseinfo.startTime+courseinfo.duration*60,"Can't close the selection in advance.");
+        require(!courseinfo.isFinshed,"The selection is already finished.");
+        courseinfo.isFinshed=true;
     }
 
-    //for debug
-    event Test(uint256 w);
-    function test() public{
-        for(uint256 i=0;i<rankingList.length;i++){
-            emit Test(rankingList[i].amount);
-        }
-    }
 }
